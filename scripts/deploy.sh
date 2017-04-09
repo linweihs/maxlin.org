9c8a237fe34bee1abf1090ebbce07dbecb4fc7e1
linweihs <aa10101491@gmail.com>
2017-04-09 12:41:29 -0700

    check my pub key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@ echo "hello world from shell"
 # copy id_rsa public key to .ssh/authorized_keys
 
 echo $RSA_PUB_KEY >> ~/.ssh/authorized_keys
+echo $RSA_PUB_KEY
 echo `pwd`
 scp -o StrictHostKeyChecking=no ./scrips/deploy.sh travis_ci@45.56.87.220:~/
 
