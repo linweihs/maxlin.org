2a680ad06f47b6e3229e8a89b34c8ad9aa4dda61
linweihs <aa10101491@gmail.com>
2017-04-09 13:44:19 -0700

    change output id_rsa path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,7 @@ set -ev
 echo "hello world from shell"
 # copy id_rsa public key to .ssh/authorized_keys
 
-chmod 400 ~/.ssh/id_rsa.pub
-echo `cat ~/.ssh/id_rsa.pub`
+echo `cat ./id_rsa.pub`
 echo `pwd`
-scp -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub -o RSAAuthentication=yes -o PubkeyAuthentication=yes ./scrips/deploy.sh travis_ci@45.56.87.220:~/
+scp -o StrictHostKeyChecking=no -i ./id_rsa.pub -o RSAAuthentication=yes -o PubkeyAuthentication=yes ./scrips/deploy.sh travis_ci@45.56.87.220:~/
 
