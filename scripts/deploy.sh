a2edbe7cb0a27573108266ab9489eac2293f98e7
linweihs <aa10101491@gmail.com>
2017-04-09 13:46:21 -0700

    chmod 400 on id_rsa

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ set -ev
 
 echo "hello world from shell"
 # copy id_rsa public key to .ssh/authorized_keys
-
+chmod 400 ./id_rsa.pub
 echo `cat ./id_rsa.pub`
 echo `pwd`
 scp -o StrictHostKeyChecking=no -i ./id_rsa.pub -o RSAAuthentication=yes -o PubkeyAuthentication=yes ./scrips/deploy.sh travis_ci@45.56.87.220:~/
