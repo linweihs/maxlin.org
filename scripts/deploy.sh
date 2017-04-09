62d109b04043a78237e54e446633b162a4936095
linweihs <aa10101491@gmail.com>
2017-04-09 11:45:53 -0700

    testing deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # copy rsa public key to .ssh/authorized_keys
-/bin/echo $RSA_PUB_KEY >> .ssh/authorized_keys
+#/bin/echo $RSA_PUB_KEY >> .ssh/authorized_keys
 
 # test deployment
-/usr/bin/scp /scripts/deploy.sh travis_ci@45.56.87.220:~/
+#/usr/bin/scp /scripts/deploy.sh travis_ci@45.56.87.220:~/
+
+/bin/echo "hello world"
