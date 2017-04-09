e8a3349d26262b06ec4bb0365f8f91626726e87b
linweihs <aa10101491@gmail.com>
2017-04-09 12:59:46 -0700

    create id_rsa key

--- scripts/deploy.rb
+++
@@ -1,2 +0,0 @@
-# test
-puts "Hello world"
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,7 @@ set -ev
 echo "hello world from shell"
 # copy id_rsa public key to .ssh/authorized_keys
 
-echo $RSA_PUB_KEY >> ~/.ssh/authorized_keys
-echo $RSA_PUB_KEY
+echo $RSA_PUB_KEY >> ~/.ssh/id_rsa.pub
 echo `pwd`
 scp -o StrictHostKeyChecking=no -o RSAAuthentication=yes ./scrips/deploy.sh travis_ci@45.56.87.220:~/
 
