3470804cfc6a2328c6bf6655b472e3ff8bcbdcf0
linweihs <aa10101491@gmail.com>
2017-04-09 13:03:09 -0700

    test scp options

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@ echo "hello world from shell"
 # copy id_rsa public key to .ssh/authorized_keys
 
 echo $RSA_PUB_KEY >> ~/.ssh/id_rsa.pub
+echo `cat ~/.ssh/id_rsa.pub`
 echo `pwd`
-scp -o StrictHostKeyChecking=no -o RSAAuthentication=yes ./scrips/deploy.sh travis_ci@45.56.87.220:~/
+scp -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub ./scrips/deploy.sh travis_ci@45.56.87.220:~/
 
