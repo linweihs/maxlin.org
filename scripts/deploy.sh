0ab76e4a2f312d96c9d2cd867e5e08e222504365
linweihs <aa10101491@gmail.com>
2017-04-10 19:12:48 -0700

    try scp to maxlin folder

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,5 @@ echo "hello world from shell"
 # copy id_rsa public key to cwd
 chmod 400 ./id_rsa
 chmod 400 ./id_rsa.pub
-ls -la ./_site/
-scp -o StrictHostKeyChecking=no -i ./id_rsa -o RSAAuthentication=yes -o PubkeyAuthentication=yes -r ./_site travis_ci@45.56.87.220:~/
+scp -o StrictHostKeyChecking=no -i ./id_rsa -o RSAAuthentication=yes -o PubkeyAuthentication=yes -r ./_site travis_ci@45.56.87.220:/home/maxlin/
 
