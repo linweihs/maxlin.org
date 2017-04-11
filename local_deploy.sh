f99f15fdfa93e46692e9973dfb485934c76cb997
linweihs <aa10101491@gmail.com>
2017-04-10 19:20:45 -0700

    run script remotely

---
+++ local_deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -ev
+
+sudo cp -r ./_site /home/maxlin/
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,5 @@ echo "hello world from shell"
 # copy id_rsa public key to cwd
 chmod 400 ./id_rsa
 chmod 400 ./id_rsa.pub
-scp -o StrictHostKeyChecking=no -i ./id_rsa -o RSAAuthentication=yes -o PubkeyAuthentication=yes -r ./_site travis_ci@45.56.87.220:/home/maxlin/
-
+scp -o StrictHostKeyChecking=no -i ./id_rsa -o RSAAuthentication=yes -o PubkeyAuthentication=yes -r ./_site travis_ci@45.56.87.220:~/
+ssh -i ./id_rsa travis_ci@45.56.87.220 'bash -s' < ./local_deploy.sh
