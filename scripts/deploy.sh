1b8dbf00f88cb55dacf3502be7ebfe5425aa3463
linweihs <aa10101491@gmail.com>
2017-04-10 19:01:38 -0700

    scp _site dir to remote

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ echo "hello world from shell"
 # copy id_rsa public key to cwd
 chmod 400 ./id_rsa
 chmod 400 ./id_rsa.pub
-echo `ls -la ./_site/`
-scp -o StrictHostKeyChecking=no -i ./id_rsa -o RSAAuthentication=yes -o PubkeyAuthentication=yes ./scripts/deploy.sh travis_ci@45.56.87.220:~/
+ls -la ./_site/
+scp -o StrictHostKeyChecking=no -i ./id_rsa -o RSAAuthentication=yes -o PubkeyAuthentication=yes -r ./_site travis_ci@45.56.87.220:~/
 
