42b82fceb6c90d503a4d2d3e3b497cbbbd4f226e
linweihs <aa10101491@gmail.com>
2017-04-10 18:53:43 -0700

    list _site verifying for deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,9 @@
 set -ev
 
 echo "hello world from shell"
-# copy id_rsa public key to .ssh/authorized_keys
+# copy id_rsa public key to cwd
 chmod 400 ./id_rsa
 chmod 400 ./id_rsa.pub
-echo `cat ./id_rsa.pub`
-echo `pwd`
+echo `ls -la ./_site/`
 scp -o StrictHostKeyChecking=no -i ./id_rsa -o RSAAuthentication=yes -o PubkeyAuthentication=yes ./scripts/deploy.sh travis_ci@45.56.87.220:~/
 
